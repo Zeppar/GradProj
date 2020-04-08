209c70350dd11b8abcba3b66cc74038563c9d3ae
BCSX <3185835784@qq.com>
2020-04-08 15:09:07 +0800

    UI Skin

---
+++ Assets/Script/Item/LevelWin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelWin : MonoBehaviour
+{
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameManager.instance.player.dead = true;
+       // UIManger.instance.winPanel.SetActive(true);
+    }
+}
--- Assets/Script/Mangers/GameManager.cs
+++ Assets/Script/Mangers/GameManager.cs
@@ -27,16 +27,15 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
-        skillManager.InitSkill();//初始化技能
-             
-      goodManger.AddItemToPanel(GoodInfo.GoodType.Skill, 0,5);//测试！！！  创建两个物品用于测试
-      goodManger.AddItemToPanel(GoodInfo.GoodType.Skill, 2,2);
-      goodManger.AddItemToPanel(GoodInfo.GoodType.Skill, 1,3);
+        skillManager.InitSkill();//初始化技能         
+        UIManger.instance.Init();
 
-        
-       //gameSaveManger.SerializeObject(goodManger.goodInfoList);
-        
-       // print(gameSaveManger.UnSerializeObject<BagItem>());
+        goodManger.AddItemToPanel(GoodInfo.GoodType.Skill, 0, 5);//测试！！！  创建两个物品用于测试
+        goodManger.AddItemToPanel(GoodInfo.GoodType.Skill, 2, 2);
+        goodManger.AddItemToPanel(GoodInfo.GoodType.Skill, 1, 3);
+        //gameSaveManger.SerializeObject(goodManger.goodInfoList);
+
+        // print(gameSaveManger.UnSerializeObject<BagItem>());
     }
 
 }
