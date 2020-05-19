e987a8324337cea1d5eb4f9e3a56cd8b0b92364d
BCSX <3185835784@qq.com>
2020-05-19 19:57:48 +0800

    Skip the Anim

--- Assets/Script/UI/GameBegining.cs
+++ Assets/Script/UI/GameBegining.cs
@@ -2,20 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class GameBegining : MonoBehaviour
 {
     public List<string> Words;
     public Text text;
-    public GameManager gameManager;
+    public UILevelManager uiLeveManager;
+
+    int count;
     void Start()
     {
              //print("set1");
             StartCoroutine(SetText());
 
     }
+    void Update(){
 
+        if (Input.GetKey(KeyCode.Space))
+        {
+            count++;
+            Debug.Log(count);
+            if (count >= 70)
+            {
+                uiLeveManager.LevelUp();
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+           
+            count = 0;
+        }
+    }
+    
     IEnumerator SetText()
     {
         foreach (var item in Words)
@@ -27,7 +47,7 @@ public class GameBegining : MonoBehaviour
             text.CrossFadeAlpha(0, 1, true);
             yield return new WaitForSeconds(1.5f);
         }
-        gameManager.LevelUp();
+        uiLeveManager.LevelUp();
     
     }
 
